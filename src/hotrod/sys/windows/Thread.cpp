bab9707d2cad65ca56c9bf85cb63f548e0e5c8fe
Ion Savin <comp_@gmx.net>
2013-10-24 22:33:25 +0300

    HRCPP-50 Thread.cpp compilation issue C2440 using MSVC12
    Make explicit the conversion to bool for the impl.

--- src/hotrod/sys/windows/Thread.cpp
+++ src/hotrod/sys/windows/Thread.cpp
@@ -204,7 +204,7 @@ Thread::Thread(Runnable* runnable) : impl(ThreadPrivate::createThread(runnable))
 Thread::Thread(Runnable& runnable) : impl(ThreadPrivate::createThread(&runnable)) {}
 
 Thread::operator bool() {
-    return impl;
+    return (bool) impl;
 }
 
 bool Thread::operator==(const Thread& t) const {
