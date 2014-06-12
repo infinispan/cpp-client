0f925daa4ffe49813c1cf0f1ba3a60ee21679ed1
rvansa <rvansa@redhat.com>
2014-06-12 07:19:11 +0200

    HRCPP-151 Do not use STL classes in public API * Using strncpy_s, getenv_s and vsnprintf_s * flag NOENABLE_INTERNAL_TESTING for not building unit tests

--- include/infinispan/hotrod/portable.h
+++ include/infinispan/hotrod/portable.h
@@ -19,6 +19,29 @@ namespace infinispan {
 namespace hotrod {
 namespace portable {
 
+#ifdef _MSC_VER
+#   define strncpy_safe strncpy_s    
+#else
+#   ifndef _TRUNCATE
+#       define _TRUNCATE ((size_t)-1)
+#   endif // _TRUNCATE
+static int strncpy_safe(char *dest, size_t numberOfElements, const char *src, size_t count) {    
+    if (!count) return 0;
+    if (!dest || !src || !numberOfElements) return -1;
+    size_t end = count != _TRUNCATE && count < numberOfElements ? count : numberOfElements - 1;
+    size_t i = 0;
+    for (; i < end && src[i]; ++i) {
+        dest[i] = src[i];        
+    }
+    if (!src[i] || end == count || count == _TRUNCATE) {
+        dest[i] = '\0';
+        return 0;
+    }
+    dest[0] = '\0';
+    return -1;
+}
+#endif
+
 #define PORTABLE_STRING_BUF_SIZE 48
 
 class HR_EXTERN string {
@@ -31,11 +54,11 @@ private:
     {
         m_length = len;
         if (len < PORTABLE_STRING_BUF_SIZE) {
-            strncpy(m_buf, str, PORTABLE_STRING_BUF_SIZE);
+            strncpy_safe(m_buf, PORTABLE_STRING_BUF_SIZE, str, len);
             m_dynamic = 0;
         } else {
             m_dynamic = new char[len + 1];
-            strncpy(m_dynamic, str, len + 1);
+            strncpy_safe(m_dynamic, len + 1, str, len);
         }
     }
 public:
