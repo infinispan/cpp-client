8c05a8a75f9188a3dc7b3641ffa0fdbc8fe802b6
Vittorio Rigamonti <Vittorio Rigamonti>
2016-06-10 11:42:03 +0200

    HRCPP-276 Updated build with static protobuf 2.6.1

--- build.sh
+++ build.sh
@@ -14,18 +14,41 @@ rm -rf infinispan-server-${INFINISPAN_VERSION}
 unzip -q infinispan-server-${INFINISPAN_VERSION}-bin.zip
 export JBOSS_HOME=`pwd`/infinispan-server-${INFINISPAN_VERSION}
 cp test/data/* $JBOSS_HOME/standalone/configuration
+
+if [  "${PROTOBUF_LIBRARY}" != "" ]
+then
+  CMAKE_EXTRAS="${CMAKE_EXTRAS} -DPROTOBUF_LIBRARY=${PROTOBUF_LIBRARY}"
+fi
+
+if [  "${PROTOBUF_PROTOC_LIBRARY}" != "" ]
+then
+  CMAKE_EXTRAS="${CMAKE_EXTRAS} -DPROTOBUF_PROTOC_LIBRARY=${PROTOBUF_PROTOC_LIBRARY}"
+fi
+
+if [  "$PROTOBUF_PROTOC_EXECUTABLE" != "" ]
+then
+  CMAKE_EXTRAS="${CMAKE_EXTRAS} -DPROTOBUF_PROTOC_EXECUTABLE=${PROTOBUF_PROTOC_EXECUTABLE}"
+fi
+
+if [  "$PROTOBUF_INCLUDE_DIR" != "" ]
+then
+  CMAKE_EXTRAS="${CMAKE_EXTRAS} -DPROTOBUF_INCLUDE_DIR=${PROTOBUF_INCLUDE_DIR}"
+fi
+
 if [  "$1" == "DEBUG" ]
 then
   rm -rf ${BUILD_DIR} &&
   mkdir ${BUILD_DIR} &&
   cd ${BUILD_DIR} &&
-  cmake -DCMAKE_BUILD_TYPE=Debug .. &&
+  echo cmake -DCMAKE_BUILD_TYPE=Debug ${CMAKE_EXTRAS}.. &&
+  cmake -DCMAKE_BUILD_TYPE=Debug ${CMAKE_EXTRAS}.. &&
   cmake --build .
 else
   rm -rf ${BUILD_DIR} &&
   mkdir ${BUILD_DIR} &&
   cd ${BUILD_DIR} &&
-  cmake .. &&
+  echo cmake ${CMAKE_EXTRAS} .. &&
+  cmake ${CMAKE_EXTRAS} .. &&
   cmake --build . &&
   ctest -V &&
   cpack -G RPM
