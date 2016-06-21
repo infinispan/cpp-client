b81eb626448746e8f8bc4679ccc0b2e67872f328
Vittorio Rigamonti <rigazilla@gmail.com>
2016-06-21 12:54:38 +0200

    HRCPP-265 TSL SChannel implementation

---
+++ src/hotrod/impl/transport/tcp/SChannelTcpTransport.h
@@ -0,0 +1,34 @@
+#ifndef ISPN_HOTROD_SCHANNELTCPTRANSPORT_H
+#define ISPN_HOTROD_SCHANNELTCPTRANSPORT_H
+
+#include <infinispan/hotrod/InetSocketAddress.h>
+#include <sstream>
+
+#include "infinispan/hotrod/defs.h"
+#include "hotrod/impl/transport/AbstractTransport.h"
+#include "hotrod/impl/transport/tcp/Socket.h"
+#include "TcpTransport.h"
+
+namespace infinispan {
+	namespace hotrod {
+		namespace transport {
+
+			class TransportFactory;
+
+			class SChannelTcpTransport : public TcpTransport
+			{
+			public:
+				SChannelTcpTransport(const InetSocketAddress& address, TransportFactory& factory, const std::string& _serverCAPath, const std::string& _serverCAFile, const std::string& _clientCertificateFile);
+
+			protected:
+				//Testing only!
+				SChannelTcpTransport();
+
+				friend class TcpTransportFactory;
+			};
+
+		}
+	}
+} // namespace infinispan::hotrod::transport::tcp
+
+#endif  /* ISPN_HOTROD_SCHANNELTCPTRANSPORT_H */
---
+++ src/hotrod/sys/SChannelSocket.h
@@ -0,0 +1,81 @@
+#ifndef ISPN_HOTROD_SYS_SCHANNELSOCKET_H
+#define ISPN_HOTROD_SYS_SCHANNELSOCKET_H
+
+#define SECURITY_WIN32
+
+#include <winsock.h>
+#include <wincrypt.h>
+#include <wintrust.h>
+#include <schannel.h>
+#include <security.h>
+#include <sspi.h>
+#include <hotrod/sys/Socket.h>
+
+namespace infinispan {
+	namespace hotrod {
+		namespace sys {
+
+			class SChannelSocket : public infinispan::hotrod::sys::Socket
+			{
+			public:
+				SChannelSocket(const std::string& _serverCAPath, const std::string& _serverCAFile, const std::string& _clientCertificateFile);
+				virtual ~SChannelSocket();
+				// Inherited via Socket
+				static SChannelSocket*  create(const std::string& _serverCAPath, const std::string& _serverCAFile, const std::string& _clientCertificateFile);
+
+				virtual void    connect(const std::string & host, int port, int timeout) override;
+				virtual void    close() override;
+				virtual void    setTcpNoDelay(bool tcpNoDelay) override;
+				virtual void    setTimeout(int timeout) override;
+				virtual size_t  read(char *p, size_t n) override;
+				virtual void    write(const char *p, size_t n) override;
+				virtual int     getSocket() override;
+
+			private:
+				static void     displayWinSockError(DWORD ErrCode);
+				static void     displayWinVerifyTrustError(DWORD Status);
+				static INT      connectToServer(std::string host, int iPortNumber, SOCKET * pSocket);
+				static DWORD    verifyServerCertificate(PCCERT_CONTEXT pServerCert, std::string host, DWORD dwCertFlags);
+				static void     logAndThrow(const std::string& host, const int port, const std::string& msg);
+
+				SECURITY_STATUS readDecrypt(const DWORD bufsize, size_t *read_counter);
+				DWORD           encryptSend(size_t len, SecPkgContext_StreamSizes Sizes);
+				SECURITY_STATUS clientHandshakeLoop(BOOL doInitialRead, SecBuffer  *pExtraData);
+				void            getNewClientCredentials();
+				SECURITY_STATUS performClientHandshake(std::string host, SecBuffer  *pExtraData);
+				void            cleanup();
+				void            displayConnectionInfo();
+
+				class SChannelInitializer
+				{
+				public:
+					SChannelInitializer();
+					PSecurityFunctionTable g_pSSPI;
+				};
+
+				static SChannelInitializer initializer;
+				static HCERTSTORE hMyCertStore;
+
+				PCCERT_CONTEXT pRemoteCertContext = NULL;
+				std::string    m_serverCAPath;
+				std::string    m_serverCAFile;
+				std::string    m_clientCertificateFile;
+				SOCKET            Client_Socket;
+				CredHandle        hCred;
+				struct _SecHandle hCtxt;
+				CtxtHandle   hContext;
+				PBYTE        pbRBuffer;
+				PBYTE        pbWBuffer;
+				PBYTE        recvBuff;
+				DWORD        cbRBuffer;
+				unsigned int ready_bytes=0;
+				unsigned int offset_bytes=0;
+				boolean      isContextInitialized = false;
+				boolean      isCredsInitialized = false;
+				boolean      onlyVerified = false;
+			};
+
+		}
+	}
+}
+#endif  /* ISPN_HOTROD_SYS_SCHANNELSOCKET_H */
