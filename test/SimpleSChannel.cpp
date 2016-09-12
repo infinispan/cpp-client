92709a0758c844c9c7327eea745a525f276a998a
Vittorio Rigamonti <rigazilla@gmail.com>
2016-09-12 12:19:15 +0200

    HRCPP-305 Fixed compile error

--- test/SimpleSChannel.cpp
+++ test/SimpleSChannel.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 	ConfigurationBuilder builder;
-	builder.addServer().host("127.0.0.1").port(11222).protocolVersion(Configuration::PROTOCOL_VERSION_24);
+	builder.protocolVersion(Configuration::PROTOCOL_VERSION_24).addServer().host("127.0.0.1").port(11222);
 	builder.ssl().enable().serverCAFile(argv[1]);
 	if (argc > 2) {
 		std::cout << "Using " << argv[1] << " as certificate" << std::endl;
