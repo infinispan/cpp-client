599f9447ae7df08753ec26bc5c7776076b6cf8b7
Vittorio <rigazilla@gmail.com>
2017-08-31 11:13:47 +0200

    added new test to check XML report result

---
+++ xunit-test/ClearTest/ClearTest.h
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+#include <memory>
+
+namespace infinispan {
+namespace hotrod
+{
+class RemoteCacheManager;
+}
+}
+
+// The fixture for testing class Foo.
+
+class ClearTest : public ::testing::Test {
+public:
+	static std::unique_ptr<infinispan::hotrod::RemoteCacheManager> remoteCacheManager;
+
+protected:
+
+    // You can do set-up work for each test here.
+    ClearTest();
+
+    // You can do clean-up work that doesn't throw exceptions here.
+    virtual ~ClearTest() {} ;
+
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
+
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+    virtual void SetUp();
+
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+    virtual void TearDown();
+
+};
