09b5a04ea3215cd9be2e5d656e72d14f9d580b9b
Vittorio <rigazilla@gmail.com>
2017-09-22 17:43:35 +0200

    added missing files

---
+++ src/hotrod/impl/operations/GetAllOperation.cpp
@@ -0,0 +1,41 @@
+#include "hotrod/impl/operations/GetAllOperation.h"
+
+namespace infinispan {
+namespace hotrod {
+namespace operations {
+
+using namespace infinispan::hotrod::protocol;
+using namespace infinispan::hotrod::transport;
+
+GetAllOperation::GetAllOperation(const Codec& _codec, std::shared_ptr<transport::TransportFactory> _transportFactory, const std::set<std::vector<char>>& keySet,
+	    const std::vector<char>& _cacheName, Topology& _topologyId, uint32_t _flags)
+        : RetryOnFailureOperation<std::map<std::vector<char>, std::vector<char>>>(_codec, _transportFactory, _cacheName, _topologyId, _flags), keySet(keySet) {}
+
+
+std::map<std::vector<char>, std::vector<char>> GetAllOperation::executeOperation(infinispan::hotrod::transport::Transport& transport)
+{
+	std::map<std::vector<char>, std::vector<char>> result;
+	TRACE("Executing GetAll(flags=%u)", flags);
+	std::unique_ptr<protocol::HeaderParams> params(this->writeHeader(transport, GET_ALL_REQUEST));
+	transport.writeVInt(keySet.size());
+	for (auto &item : keySet)
+	{
+		transport.writeArray(item);
+		transport.flush();
+	}
+	uint8_t status = readHeaderAndValidate(transport, *params);
+	if (status == NO_ERROR_STATUS) {
+		uint32_t count = transport.readVInt();
+		for (uint32_t i = 0; i < count; i++)
+				{
+			std::vector<char> k = transport.readArray();
+			std::vector<char> v = transport.readArray();
+			result[k] = v;
+		}
+	} else {
+		TRACE("Error status %u", status);
+	}
+return result;
+}
+
+}}}
---
+++ src/hotrod/impl/operations/GetAllOperation.h
@@ -0,0 +1,28 @@
+#ifndef ISPN_HOTROD_OPERATIONS_GETALLOPERATION_H
+#define ISPN_HOTROD_OPERATIONS_GETALLOPERATION_H
+
+
+#include "infinispan/hotrod/RemoteCacheBase.h"
+#include "hotrod/impl/operations/RetryOnFailureOperation.h"
+
+namespace infinispan {
+namespace hotrod {
+class Topology;
+namespace operations {
+
+class GetAllOperation : public RetryOnFailureOperation<std::map<std::vector<char>, std::vector<char> > >
+{
+  protected:
+    std::map<std::vector<char>, std::vector<char>> executeOperation(
+        infinispan::hotrod::transport::Transport& transport);
+
+  private:
+    GetAllOperation(const Codec& _codec, std::shared_ptr<transport::TransportFactory> _transportFactory, const std::set<std::vector<char>>& keySet,
+    	    const std::vector<char>& _cacheName, Topology& _topologyId, uint32_t _flags);
+    const std::set<std::vector<char>>& keySet;
+  friend class OperationsFactory;
+};
+
+}}} // namespace infinispan::hotrod::operations
+
+#endif  // ISPN_HOTROD_OPERATIONS_GETALLOPERATION_H
