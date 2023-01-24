4c657be9cd70c17499b04dc2a0c9a9ca52e275c3
rigazilla <vrigamon@redhat.com>
2023-01-24 16:35:34 +0100

    New exec with key operation

--- src/hotrod/impl/operations/ExecuteCmdOperation.h
+++ src/hotrod/impl/operations/ExecuteCmdOperation.h
@@ -1,40 +1,66 @@
 #ifndef ISPN_HOTROD_OPERATIONS_EXECUTECMDOPERATION_H
 #define ISPN_HOTROD_OPERATIONS_EXECUTECMDOPERATION_H
 
-
-
 #include "hotrod/impl/operations/RetryOnFailureOperation.h"
-
+#include "hotrod/impl/operations/AbstractKeyOperation.h"
 
 #include <set>
 
-namespace infinispan {
-namespace hotrod {
-class Topology;
-namespace operations {
-
-class ExecuteCmdOperation : public RetryOnFailureOperation<std::vector<char> >
+namespace infinispan
 {
-protected:
-    ExecuteCmdOperation(
-            const infinispan::hotrod::protocol::Codec&       codec_,
-            std::shared_ptr<transport::TransportFactory> transportFactory_,
-            const std::vector<char>&                                   cacheName_,
-            Topology&                                 topologyId_,
-            uint32_t                                         flags_,
-            const std::vector<char>&                                   cmdName_,
-            const std::map<std::vector<char>,std::vector<char>>&   cmdArgs_,
-            EntryMediaTypes* df);
-            std::vector<char> sendExecuteOperation(
-                    transport::Transport&     transport,
-					uint8_t                                       opCode,
-					uint8_t                                       opRespCode);
-	std::vector<char> executeOperation(infinispan::hotrod::transport::Transport& transport);
-    const std::vector<char>& cmdName;
-    const std::map<std::vector<char>,std::vector<char>>& cmdArgs;
-    friend class OperationsFactory;
-};
+    namespace hotrod
+    {
+        class Topology;
+        namespace operations
+        {
 
-}}} // namespace
+            class ExecuteCmdOperation : public RetryOnFailureOperation<std::vector<char>>
+            {
+            protected:
+                ExecuteCmdOperation(
+                    const infinispan::hotrod::protocol::Codec &codec_,
+                    std::shared_ptr<transport::TransportFactory> transportFactory_,
+                    const std::vector<char> &cacheName_,
+                    Topology &topologyId_,
+                    uint32_t flags_,
+                    const std::vector<char> &cmdName_,
+                    const std::map<std::vector<char>, std::vector<char>> &cmdArgs_,
+                    EntryMediaTypes *df);
+                std::vector<char> sendExecuteOperation(
+                    transport::Transport &transport,
+                    uint8_t opCode,
+                    uint8_t opRespCode);
+                std::vector<char> executeOperation(infinispan::hotrod::transport::Transport &transport);
+                const std::vector<char> &cmdName;
+                const std::map<std::vector<char>, std::vector<char>> &cmdArgs;
+                friend class OperationsFactory;
+            };
 
-#endif  /* ISPN_HOTROD_EXECUTECMDOPERATION_H */
+            class ExecuteCmdKeyOperation : public AbstractKeyOperation<std::vector<char>>
+            {
+            protected:
+                ExecuteCmdKeyOperation(
+                    const infinispan::hotrod::protocol::Codec &codec_,
+                    std::shared_ptr<transport::TransportFactory> transportFactory_,
+                    const std::vector<char> &key_,
+                    const std::vector<char> &cacheName_,
+                    Topology &topologyId_,
+                    uint32_t flags_,
+                    const std::vector<char> &cmdName_,
+                    const std::map<std::vector<char>, std::vector<char>> &cmdArgs_,
+                    EntryMediaTypes *df);
+                std::vector<char> sendExecuteOperation(
+                    transport::Transport &transport,
+                    uint8_t opCode,
+                    uint8_t opRespCode);
+                std::vector<char> executeOperation(infinispan::hotrod::transport::Transport &transport);
+                const std::vector<char> &cmdName;
+                const std::map<std::vector<char>, std::vector<char>> &cmdArgs;
+                friend class OperationsFactory;
+            };
+
+        }
+    }
+} // namespace
+
+#endif /* ISPN_HOTROD_EXECUTECMDOPERATION_H */
